ce1558c7793916b6225a27faf8fc8228cb5e0cd3
Tharuka-Sandaruwan <ts2000614@gmail.com>
2022-03-30 22:36:00 +0530

    email validator created

---
+++ src/Classes/testClass.java
@@ -0,0 +1,10 @@
+
+package Classes;
+import Classes.dataValidator;
+
+public class testClass {
+    public static void main(String[] args) {
+        
+        System.out.println(dataValidator.eMailValidator("as_da.sds.d.lk@rrk.kj"));
+    }
+}
