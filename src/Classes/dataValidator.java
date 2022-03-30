53c5eacff78a230a8fa1d387e8b1212de59bde9f
Tharuka-Sandaruwan <ts2000614@gmail.com>
2022-03-30 21:33:41 +0530

    nic validator created

--- src/Classes/dataValidator.java
+++ src/Classes/dataValidator.java
@@ -6,4 +6,21 @@ package Classes;
 
 public class dataValidator {
     
+    public static boolean NICvalidator(String nicNumber){
+    if (nicNumber.length() == 12 && nicNumber.matches("[0-9]+") &&
+            Integer.parseInt(nicNumber.substring(4, 7)) < 867){
+     return  true;
+    }
+      else if (nicNumber.length() == 10 && nicNumber.indexOf("V") == 9 && 
+              nicNumber.matches("[0-9]+(.*)V") && 
+              Integer.parseInt(nicNumber.substring(2, 5)) < 867){
+       return true;
+      }
+      else{
+              return  false;
+
+      }
+    }
+    
+    
 }
